d3d440f0d3934251bf2f757bf4190d3bf4ae8931
mahdiJihad <mahdijihad001@gmail.com>
2025-03-29 20:00:41 +0600

    Implement User Authentication

--- src/Middleware/genatareToken.js
+++ src/Middleware/genatareToken.js
@@ -1,8 +1,12 @@
-const { userModel } = require("../User/userModel")
+const { userModel } = require("../User/userModel");
+const jwt = require("jsonwebtoken");
 
 const genarateToken = async(userId) =>{
     const findUser = await userModel.findById(userId);
-    
-    
+    const token = jwt.sign({user_id : findUser._id , role : findUser.role} , process.env.jwt_secrate_key , {expiresIn : "48h"})
+    return token
+};
 
-}
+
+
+module.exports = {genarateToken}
