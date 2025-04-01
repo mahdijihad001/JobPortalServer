d4102619c6cf10d792d814c00bfb2b316cc9a56d
mahdiJihad <mahdijihad001@gmail.com>
2025-04-01 07:29:09 +0600

    Complite user api

--- src/User/userRouter.js
+++ src/User/userRouter.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { createUserController, getAllUserController, postUserLoginController, updateUserProfile, findSingleUser } = require("./userController");
+const { createUserController, getAllUserController, postUserLoginController, updateUserProfile, findSingleUser, logOutUser } = require("./userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/create" , createUserController);
 userRouter.get("/find" , getAllUserController);
 userRouter.post("/login" , postUserLoginController);
-userRouter.post("/profileUpdate/:id" , updateUserProfile);
+userRouter.patch("/profileUpdate/:id" , updateUserProfile);
 userRouter.get("singleUser/:id" , findSingleUser);
+userRouter.delete("/delete/:id");
+userRouter.post("/logout" , logOutUser);
 
 module.exports = {userRouter};
