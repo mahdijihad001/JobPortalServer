5f13b860d9f629139ef048214e813329aeec876e
mahdiJihad <mahdijihad001@gmail.com>
2025-04-01 06:57:37 +0600

    Integrate get single user

--- src/User/userController.js
+++ src/User/userController.js
@@ -1,116 +1,141 @@
 const { genarateToken } = require("../Middleware/genatareToken");
 const { userModel } = require("./userModel");
+const mongoose = require("mongoose");
 
-const createUserController = async(req , res) =>{
-   try {
-    const result = await userModel({...req.body});
-    await result.save();
+const createUserController = async (req, res) => {
+    try {
+        const result = await userModel({ ...req.body });
+        await result.save();
 
-    res.status(201).send({
-        success : true,
-        message : "User resistraction success"
-    })
+        res.status(201).send({
+            success: true,
+            message: "User resistraction success"
+        })
 
-   } catch (error) {
-    return res.status(400).send({
-        success : false,
-        message : "Invalid Data",
-        error : error.message
-    })
-   }
+    } catch (error) {
+        return res.status(400).send({
+            success: false,
+            message: "Invalid Data",
+            error: error.message
+        })
+    }
 };
 
-const postUserLoginController = async(req , res) =>{
+const postUserLoginController = async (req, res) => {
     try {
-        
-        const {username , password} = req.body;
-        // Find User
-        const findUser = await userModel.findOne({username : username});
-        const sendUser = await userModel.findOne({username : username} , {password : 0 , __v : 0});
 
-        if(!findUser){
+        const { username, password } = req.body;
+        // Find User
+        const findUser = await userModel.findOne({ username: username });
+        const sendUser = await userModel.findOne({ username: username }, { password: 0, __v: 0 });
+
+        if (!findUser) {
             return res.status(404).send({
-                success : false,
-                message : "User not valid"
+                success: false,
+                message: "User not valid"
             })
         };
-       
-        // Match password
-       const matchPassword = await findUser.compairePassword(password);
 
-       if(!matchPassword){
-        return res.status(400).send({
-            success : false,
-            message : "Please type valid information!"
+        // Match password
+        const matchPassword = await findUser.compairePassword(password);
+
+        if (!matchPassword) {
+            return res.status(400).send({
+                success: false,
+                message: "Please type valid information!"
+            })
+        }
+        //    Genarate Token    
+        const token = await genarateToken(findUser._id)
+        //    Set Cookie
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: 'None',
+            secure: true,
         })
-       }
-    //    Genarate Token    
-       const token = await genarateToken(findUser._id)
-    //    Set Cookie
-       res.cookie("token" , token , {
-        httpOnly: true,
-        sameSite: 'None',
-        secure: true,
-       })
 
         res.status(200).send({
-            success : true,
-            message : "Login Success!",
+            success: true,
+            message: "Login Success!",
             token,
-            user : sendUser
+            user: sendUser
         })
 
 
     } catch (error) {
         return res.status(400).send({
-            success : false,
-            message : "User not valid"
+            success: false,
+            message: "User not valid"
         })
     }
 }
 
-const getAllUserController = async(req , res) =>{
+const getAllUserController = async (req, res) => {
     try {
         const result = await userModel.find({});
 
         res.status(200).send({
-            success : true,
-            message : "Get all user",
-            data : result
+            success: true,
+            message: "Get all user",
+            data: result
         })
 
     } catch (error) {
         return res.status(404).send({
-            success : false,
-            message : "No user found"
+            success: false,
+            message: "No user found"
         })
     }
 }
 
 
-const updateUserProfile = async(req , res) =>{
+const updateUserProfile = async (req, res) => {
     try {
         const id = req.params.id;
-        const result = await userModel.findByIdAndUpdate(id , req.body , {new : true})
+        const result = await userModel.findByIdAndUpdate(id, req.body, { new: true })
 
-        if(!result){
+        if (!result) {
             return res.status(404).send({
-                success : false,
-                message : "User not found"
+                success: false,
+                message: "User not found"
             })
         };
 
         res.status(200).send({
-            success : true , 
-            message : "User profile update success"
+            success: true,
+            message: "User profile update success"
         })
 
     } catch (error) {
         return res.status(400).send({
-            success : false,
-            message : "Bad Request! Profile not update.."
+            success: false,
+            message: "Bad Request! Profile not update.."
+        })
+    }
+};
+
+
+const findSingleUser = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).send({success : false , message : "Invalid user"});
+        }
+        const result = await userModel.findOne({ id: id });
+
+        if (!result) {
+            return res.status(400).send({success : false , message : "User not found"});
+        };
+
+        res.status(200).send({success : true  , message : "User founded!!" , data : result})
+
+    } catch (error) {
+        return res.status(404).send({
+            success: false,
+            message: "User not found!"
         })
     }
 }
 
-module.exports = {createUserController , getAllUserController , postUserLoginController , updateUserProfile};
+module.exports = { createUserController, getAllUserController, postUserLoginController, updateUserProfile , findSingleUser};
--- src/User/userRouter.js
+++ src/User/userRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { createUserController, getAllUserController, postUserLoginController, updateUserProfile } = require("./userController");
+const { createUserController, getAllUserController, postUserLoginController, updateUserProfile, findSingleUser } = require("./userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/create" , createUserController);
 userRouter.get("/find" , getAllUserController);
 userRouter.post("/login" , postUserLoginController);
-userRouter.post("/profile/:id" , updateUserProfile);
-
+userRouter.post("/profileUpdate/:id" , updateUserProfile);
+userRouter.get("singleUser/:id" , findSingleUser);
 
 module.exports = {userRouter};
