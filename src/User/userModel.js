2e062e63fb5f131b14dc82f98ee8eef9a166beb6
mahdiJihad <mahdijihad001@gmail.com>
2025-04-03 00:00:35 +0600

    Apply Registraction api

--- src/User/userModel.js
+++ src/User/userModel.js
@@ -21,8 +21,8 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum : ["candidate" , "employer" , "admin"],
-        default: "candidate"
+        // enum : ["candidate" , "employer" , "admin"],
+        // default: "candidate"
     },
     jobTitle: {
         type: String
