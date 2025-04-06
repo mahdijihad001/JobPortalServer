c31cd67674063d86d004c935ecd20d8650035815
mahdiJihad <mahdijihad001@gmail.com>
2025-04-06 22:09:58 +0600

    Build Job Releted apis

--- src/User/userModel.js
+++ src/User/userModel.js
@@ -7,6 +7,9 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique : true
     },
+    fullName : {
+        type : String,
+    },
     email: {
         type: String,
         required: true,
