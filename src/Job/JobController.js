392537ad6652e1c4b40ed104b255c7f308bafcdf
mahdiJihad <mahdijihad001@gmail.com>
2025-04-09 10:04:45 +0600

    Implement pagination

--- src/Job/JobController.js
+++ src/Job/JobController.js
@@ -1,22 +1,60 @@
 const jobModel = require("./jobModel")
 
-const createJobController = async(req , res) =>{
+const createJobController = async (req, res) => {
     try {
-        const result = await jobModel({...req.body});
+        const result = await jobModel({ ...req.body });
         await result.save();
 
-        if(!result){
-            return res.status(400).send({ success : false, message : "Job posted faild" })
+        if (!result) {
+            return res.status(400).send({ success: false, message: "Job posted faild" })
         };
 
-        res.status(200).send({success : true , message : "Job Posted success"});
+        res.status(200).send({ success: true, message: "Job Posted success" });
 
     } catch (error) {
         return res.status(400).send({
-            status : false,
-            message : "Job created faild!"
+            status: false,
+            message: "Job created faild!"
         });
     }
 };
 
-module.exports = {createJobController}
+
+const findAllJob = async (req, res) => {
+    try {
+
+        const { page = 1 , department , authore} = req.query;
+
+        const filter = {};
+        if(department){
+            filter.jobType = department
+        }
+        if(authore){
+            filter.authore = authore
+        }
+
+        const skip = (parseInt(page) - 1) * 10;
+
+        const totalJob = await jobModel.countDocuments(filter);
+
+        const findJob = await jobModel.find(filter).skip(skip).limit(10);
+        const totalpage = Math.ceil(totalJob / 10);
+
+        if (!findJob) {
+            return res.status(404).send({ success: false, message: "Job not found" });
+        };
+
+        res.status(200).send({ success: true, message: "Job found success.", data: {
+            totalJob,
+            totalpage,
+            findJob
+        }});
+
+    } catch (error) {
+        return res.status(400).send({
+            success: false, message: "Job not found",
+        });
+    }
+}
+
+module.exports = { createJobController, findAllJob }
--- src/Job/jobRouter.js
+++ src/Job/jobRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createJobController } = require("./JobController");
+const { createJobController, findAllJob } = require("./JobController");
 
 const jobRouter = express.Router();
 
 jobRouter.post("/create" , createJobController);
+jobRouter.get("/find" , findAllJob);
 
 module.exports = {jobRouter};
